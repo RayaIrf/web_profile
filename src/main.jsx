22439e4eb9c0980100102d12038d604ccfc342ca
Raya Irfansyah <xrayirf@gmail.com>
2023-02-21 02:01:17 +0700

    Add Navbar

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,13 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import App from './App'
 import './index.css'
+import Navbar from './Navbar'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<App />}></Route>
+        <Route path='/' element={<Navbar />}></Route>
       </Routes>
     </BrowserRouter>
   </React.StrictMode>,
